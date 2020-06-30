9fbaf2b00e716a765a2c90ea2a27e626b81cf13a
Jigar Tarpara <jigartarpara68@gmail.com>
2020-06-30 09:48:24 +0530

    Fix

--- setup.py
+++ setup.py
@@ -1,9 +1,7 @@
 # -*- coding: utf-8 -*-
 from setuptools import setup, find_packages
-try: # for pip >= 10
-    from pip._internal.req import parse_requirements
-except ImportError: # for pip <= 9.0.3
-    from pip.req import parse_requirements
+with open('requirements.txt') as f:
+	install_requires = f.read().strip().split('\n')
 import re, ast
 
 # get version from __version__ variable in studio/__init__.py
@@ -12,8 +10,6 @@ _version_re = re.compile(r'__version__\s+=\s+(.*)')
 with open('studio/__init__.py', 'rb') as f:
     version = str(ast.literal_eval(_version_re.search(
         f.read().decode('utf-8')).group(1)))
-
-requirements = parse_requirements("requirements.txt", session="")
 
 setup(
 	name='studio',
@@ -24,6 +20,5 @@ setup(
 	packages=find_packages(),
 	zip_safe=False,
 	include_package_data=True,
-	install_requires=[str(ir.req) for ir in requirements],
-	dependency_links=[str(ir._link) for ir in requirements if ir._link]
+	install_requires=install_requires,
 )
